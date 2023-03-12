4b53802380295d24d913be26cb62348635bd3c33
James Robinson <1500092+AverageHelper@users.noreply.github.com>
2023-03-12 13:04:56 -0600

    Basic routing (#4)
    * fix: Respond 404 to requests not at the root
    
    * feat: Respect the `Accept` header when sending 404

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,32 @@ const headers = {
 // Cloudflare wants the endpoint to be a `default` export:
 const handlers: ExportedHandler = {
 	fetch(req) {
+		// Figure out what path we're aiming for
+		let url: URL;
+		try {
+			url = new URL(req.url);
+		} catch {
+			// This should never happen
+			return new Response(undefined, { status: 500, headers });
+		}
+
+		if (url.pathname !== "/") {
+			const accept = req.headers.get("accept");
+			const status = 404;
+			let message = "Not found";
+			let contentType = "text/plain";
+
+			if (accept?.includes("application/json")) {
+				message = JSON.stringify({ status, message });
+				contentType = "application/json";
+			}
+
+			return new Response(message.concat("\n"), {
+				status,
+				headers: { ...headers, "Content-Type": contentType },
+			});
+		}
+
 		switch (req.method.toUpperCase()) {
 			// Normal requests:
 			case "GET":
