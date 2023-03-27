fdc0a0e28011830de0126f373160ecd66f7976cc
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2023-03-27 12:33:41 -0600

    chore: We don't need a whole function for our sanity test route

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import { badPath } from "./helpers/badPath";
 import { cors } from "./helpers/cors";
 import { echo } from "./routes/echo";
 import { errorHandler as handleErrors } from "./errorHandler";
-import { errorSanity } from "./routes/errorSanity";
 import { handlerFor, headFor } from "./fetchHandler";
 import { Hono } from "hono";
+import { InternalError } from "./errors/InternalError";
 
 // Testing this file involves a special Cloudflare process that doesn't do coverage
 /* istanbul ignore next */
@@ -28,7 +28,7 @@ const app = new Hono()
 	.get("/test-error", (c, next) => {
 		if (c.env?.["NODE_ENV"] === "test") {
 			// In test mode, we always throw
-			return handlerFor(errorSanity)(c, next);
+			throw new InternalError(c.res);
 		}
 
 		// Pretend this is an unknown route
