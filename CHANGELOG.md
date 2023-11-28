89ab3ecc54612a766c2d39ec7f11bb2fed159b1a
AverageHelper <me@average.name>
2023-11-28 03:17:32 +0000

    fix: CORS for /openapi.yaml (#15)
    Seems the default handler doesn't do our headers. I could probably fix that in a better way, but for now, this hack does the trick.
    
    Reviewed-on: https://codeberg.org/AverageHelper/ip-echo-cloudflare/pulls/15
    Co-authored-by: AverageHelper <me@average.name>
    Co-committed-by: AverageHelper <me@average.name>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.2.1] - 2023-11-27
+### Fixed
+- Send CORS and other appropriate headers with our API spec file.
+
 ## [1.2.0] - 2023-11-27
 ### Added
 - New endpoint `/openapi.yaml` answers `GET` requests with our API spec file.
@@ -55,6 +59,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - Cloudflare Worker to echo the caller's IP address.
 
+[1.2.1]: https://codeberg.org/AverageHelper/ip-echo-cloudflare/compare/v1.2.0...v1.2.1
 [1.2.0]: https://codeberg.org/AverageHelper/ip-echo-cloudflare/compare/v1.1.5...v1.2.0
 [1.1.5]: https://codeberg.org/AverageHelper/ip-echo-cloudflare/compare/v1.1.4...v1.1.5
 [1.1.4]: https://codeberg.org/AverageHelper/ip-echo-cloudflare/compare/v1.1.3...v1.1.4
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ import { cors } from "./helpers/cors";
 import { echo } from "./routes/echo";
 import { errorHandler as handleErrors } from "./errorHandler";
 import { handleGet } from "./fetchHandler";
+import { headers } from "./headers";
 import { Hono } from "hono";
+import { NotFoundError } from "./errors/NotFoundError";
 import { serveStatic } from "hono/cloudflare-workers";
 
 const app = new Hono<Env>();
@@ -15,8 +17,17 @@ const app = new Hono<Env>();
 handleGet(app, "/", echo);
 handleGet(app, "/about", about);
 
+// TODO: Clean this up
 app
-	.get("openapi.yaml", serveStatic({ root: ".", path: "./openapi.yaml" }))
+	.get("openapi.yaml", async c => {
+		const next = async (): Promise<void> => await Promise.resolve();
+		const res = await serveStatic({ root: ".", path: "./openapi.yaml" })(c, next);
+		if (!res) throw new NotFoundError();
+		return c.text(await res.text(), 200, {
+			...headers,
+			"Content-Type": "application/x-yaml;charset=UTF-8",
+		});
+	})
 	.options(cors)
 	.all(badMethod);
 
